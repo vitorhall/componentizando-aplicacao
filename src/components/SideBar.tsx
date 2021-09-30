8d7cb8a0b6571470fe9ec9ac08f44b31fcad5441
vitorhall <vitorhall2@gmail.com>
2021-09-30 16:33:01 -0300

    componentizando a aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import { useGenres } from "../hooks/useGenres";
+import { Button } from "./Button";
+
+
 export function SideBar() {
-  // Complete aqui
+  // Complete aqui 
+  const {selectedGenreId, setSelectedGenreId, genres} = useGenres();
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
